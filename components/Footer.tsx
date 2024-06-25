611eec3ea89909debb1b0a42a7ba55c0e2554ab5
devMapute <ammapute1@up.edu.ph>
2024-06-25 16:48:41 +0800

    chore: minor padding changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,11 +6,11 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
+    <footer className="pt-20 w-full mb-[100px] md:mb-5 pb-10" id="contact">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
+          experience to the next level?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you
